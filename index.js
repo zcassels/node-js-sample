2cd59eb08f047712c39ea66f188049a5527ba158
zcassels <zcassels@gmail.com>
2017-09-01 10:28:23 -0400

    add post

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ app.get('/', function(request, response) {
   response.send(JSON.stringify({"ip": request.connection.remoteAddress, "port": request.connection.remotePort}));
 })
 
+app.post('/', function(request, response) {
+  console.log("Request Inbound -> " + request.connection.remoteAddress + " (" + request.connection.remotePort + ")");
+  response.send(JSON.stringify({"ip": request.connection.remoteAddress, "port": request.connection.remotePort}));
+})
+
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
