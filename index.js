f06348aa530acbf9f4031bed2375e2e4e6dfff29
zcassels <zcassels@gmail.com>
2017-09-01 10:03:56 -0400

    update to print ip

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-var express = require('express')
-var app = express()
+var express = require('express');
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(request, response) {
-  response.send('Hello World!')
+  console.log("Request Inbound -> " + request.connection.remoteAddress + " (" + request.connection.remotePort + ")");
+  response.send(JSON.stringify({"ip": request.connection.remoteAddress, "port": request.connection.remotePort}));
 })
 
 app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
