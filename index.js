7bf6e292c8ee01b536e82e39317386b2d1876a26
zcassels <zcassels@gmail.com>
2017-10-06 10:46:43 -0400

    add exe to serve

--- index.js
+++ index.js
@@ -4,16 +4,6 @@ var app = express();
 app.set('port', (process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.get('/', function(request, response) {
-  console.log("Request Inbound -> " + request.connection.remoteAddress + " (" + request.connection.remotePort + ")");
-  response.send(JSON.stringify({"ip": request.connection.remoteAddress, "port": request.connection.remotePort}));
-})
-
-app.post('/', function(request, response) {
-  console.log("Request Inbound -> " + request.connection.remoteAddress + " (" + request.connection.remotePort + ")");
-  response.send(JSON.stringify({"ip": request.connection.remoteAddress, "port": request.connection.remotePort}));
-})
-
 app.listen(app.get('port'), function() {
   console.log("Node app is running at localhost:" + app.get('port'));
 });
